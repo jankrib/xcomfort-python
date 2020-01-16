2d7179c33289daee4deddb369e650a22de34116b
jankrib <jan@crondale.no>
2020-01-16 21:41:54 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="xcomfort",
+    version="0.0.1",
+    author="Jan Kristian Bjerke",
+    author_email="jan.bjerke@gmail.com",
+    description="Integration with Eaton xComfort Bridge",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jankrib/xcomfort-python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Development Status :: 2 - Pre-Alpha",
+    ],
+    python_requires='>=3.7',
+)
