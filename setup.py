01771838d3fca4676e7a12b1ae69cb11504f684e
jankrib <jan.bjerke@gmail.com>
2020-07-06 20:31:09 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="xcomfort",
-    version="0.0.7",
+    version="0.0.8",
     author="Jan Kristian Bjerke",
     author_email="jan.bjerke@gmail.com",
     description="Integration with Eaton xComfort Bridge",
