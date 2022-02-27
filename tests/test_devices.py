cd55f175c306218d9280a13b759d7ad4907b1e33
jankrib <jan.bjerke@gmail.com>
2022-02-27 11:30:53 +0100

    Introduce tests

---
+++ tests/test_devices.py
@@ -0,0 +1,30 @@
+import pytest
+from xcomfort.devices import (Light, LightState)
+
+def inc(x):
+    return x + 1
+
+
+def test_lightstate_switch_on():
+    device = Light(None, 1, "", True)
+
+    payload = {
+        "switch": True,
+        "dimmvalue": 50
+    }
+
+    device.handle_state(payload)
+
+    assert device.state.value.switch == True
+    assert device.state.value.dimmvalue == 50
+
+def test_lightstate_switch_on_when_not_dimmable():
+    device = Light(None, 1, "", False)
+
+    payload = {
+        "switch": True
+    }
+
+    device.handle_state(payload)
+
+    assert device.state.value.switch == True
