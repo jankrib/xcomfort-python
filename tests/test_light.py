b8d9b82c6c5dccda7e9172ae7fdd20d4eef678ee
jankrib <jan@crondale.no>
2022-07-18 22:16:32 +0200

    Update test_light.py

--- tests/test_light.py
+++ tests/test_light.py
@@ -1,6 +1,27 @@
 import pytest
+from mock import patch, Mock
 import json
+from xcomfort.bridge import Bridge
 from xcomfort.devices import (Light, LightState)
+from xcomfort.messages import Messages
+
+
+class MockBridge(Bridge):
+    def __init__(self):
+        self._sent_message = None
+
+    async def send_message(self, message_type, message):
+        self._sent_message = message
+
+
+@pytest.mark.asyncio
+async def test_light_switch_on():
+    bridge = MockBridge()
+    device = Light(bridge, 1, "", True)
+
+    await device.switch(True)
+
+    assert bridge._sent_message == {"deviceId": 1, "switch": True}
 
 
 def test_lightstate_switch_on():
@@ -16,6 +37,7 @@ def test_lightstate_switch_on():
     assert device.state.value.switch == True
     assert device.state.value.dimmvalue == 50
 
+
 def test_lightstate_switch_on_when_not_dimmable():
     device = Light(None, 1, "", False)
 
@@ -26,4 +48,3 @@ def test_lightstate_switch_on_when_not_dimmable():
     device.handle_state(payload)
 
     assert device.state.value.switch == True
-
